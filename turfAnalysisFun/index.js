95a8459a6a91b48d9fbdad5ee6f133fe3b6cedb9
Aissa Feldmann <aissafeldmann@Aissas-Air.attlocal.net>
2020-10-18 23:39:29 -0400

    successfully adding some isopoly coords w readFileSync

--- turfAnalysisFun/index.js
+++ turfAnalysisFun/index.js
@@ -1,10 +1,10 @@
 const turf = require('@turf/turf');
 const fs = require('fs');
 
-let patientPoints = fs.readFileSync('../input/patient-data.geojson');
+let patientPoints = fs.readFileSync('./input/patient-data.geojson');
 patientPoints = JSON.parse(patientPoints);
 
-let isoPoly = fs.readFileSync('../input/isoSixtytMin.geojson');
+let isoPoly = fs.readFileSync('./input/isoSixtyMin.geojson');
 isoPoly = JSON.parse(isoPoly);
 
 // var distance = turf.distance(patientPoints);
